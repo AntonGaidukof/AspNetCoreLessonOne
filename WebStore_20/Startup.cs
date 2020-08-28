64d9d05f5fbd8e6b03d1206f10ab5ae689048535
AntonGaidukof <antonioizioshki@gmail.com>
2020-08-28 22:03:27 +0300

    Добавил исходники

---
+++ WebStore_20/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            var helloString = _configuration["CustomHelloWorld"];
+            //var helloString = _configuration["Logging:LogLevel:Default"];
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // endpoints.MapDefaultControllerRoute(); // краткий аналог
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+// https://localhost:44317/    home            /index
+// https://localhost:44317/
+                // Маршрут по умолчанию состоит из трёх частей разделённых “/”
+                // Первой частью указывается имя контроллера,
+                // второй - имя действия (метода) в контроллере,
+                // третей - опциональный параметр с именем “id”
+                // Если часть не указана - используются значения по умолчанию:
+                // для контроллера имя “Home”,
+                // для действия - “Index”
+
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync(helloString);
+                //});
+            });
+        }
+    }
+}
